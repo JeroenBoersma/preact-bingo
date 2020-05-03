051133e67b32b862f012aaba4ffe0d7084f3e876
Jeroen Boersma <jeroen@elgentos.nl>
2020-05-03 21:19:57 +0200

    Added Stats to seperate class

--- src/lib/board/unique/game.js
+++ src/lib/board/unique/game.js
@@ -1,3 +1,36 @@
+class Stats {
+
+    game;
+
+    /**
+     *
+     * @param {Game} game
+     */
+    constructor(game) {
+        this.game = game;
+    }
+
+    get progress() {
+        return this.game.progress;
+    }
+
+    get remaining() {
+        return this.game.total - this.game.progress;
+    }
+
+    get todo() {
+        return Math.round((1 - this.game.progress / this.game.total) * 1000) / 10;
+    }
+
+    get total() {
+        return this.game.total;
+    }
+
+    get done() {
+        return Math.round(this.game.progress / this.game.total * 1000) / 10;
+    }
+}
+
 
 class Game {
 
@@ -69,8 +102,7 @@ class Game {
         return this.generator.numbers[n];
     }
 
-    done = () => Math.round(this.progress / this.total * 100, 1);
-    todo = () => Math.round((1 - this.progress / this.total) * 100, 1);
+    stats = () => new Stats(this);
 }
 
 export default Game;
