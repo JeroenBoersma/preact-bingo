640287574e511b7a89cbf6dd4942997cd8419a3f
Jeroen Boersma <jeroen@elgentos.nl>
2020-05-06 23:27:32 +0200

    Removed proxy file which was overhead

--- src/components/upnext/index.js
+++ src/components/upnext/index.js
@@ -1,13 +1,23 @@
-import {h} from 'preact';
-import { Updater } from './updater';
+import { useState } from 'preact/hooks';
+import { Stats } from './stats';
+import AutoUpdater from './autoUpdater';
+import History from './history';
 
 const UpNext = ({card}) => {
 
     const memory = card.game();
 
+    const [number, setNumber] = useState();
+
+    const updateNumber = () => setNumber(memory.next());
+
     return (
         <div class="game-stats">
-            <Updater memory={memory} />
+            <Stats stats={memory.stats()} />
+            <h1>Called <strong>{number}</strong></h1>
+            <button onClick={updateNumber}>Next</button>
+            <AutoUpdater callback={updateNumber} />
+            <History history={memory.history} />
         </div>
     );
 }
