2dbdd55a67463240574adc02899578600698a9de
Jeroen Boersma <jeroen@elgentos.nl>
2020-05-07 22:09:07 +0200

    Added New game option

--- src/components/upnext/index.js
+++ src/components/upnext/index.js
@@ -5,19 +5,24 @@ import History from './history';
 
 const UpNext = ({card}) => {
 
-    const memory = card.game();
+    const game = card.game();
 
     const [number, setNumber] = useState();
 
-    const updateNumber = () => setNumber(memory.next());
+    const updateNumber = () => setNumber(game.next());
+    const newGame = () => {
+        game.reset();
+        setNumber();
+    }
 
     return (
         <div class="game-stats">
-            <Stats stats={memory.stats()} />
+            <Stats stats={game.stats()} />
             <h1>Called <strong>{number}</strong></h1>
             <button onClick={updateNumber}>Next</button>
+            <button onClick={newGame}>New game</button>
             <AutoUpdater callback={updateNumber} />
-            <History history={memory.history} />
+            <History history={game.history} />
         </div>
     );
 }
