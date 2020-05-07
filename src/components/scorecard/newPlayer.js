914f96d28843865fa01c1b6863d3676ca40c9c3b
Jeroen Boersma <jeroen@elgentos.nl>
2020-05-07 22:09:29 +0200

    Focus correctly on input field

--- src/components/scorecard/newPlayer.js
+++ src/components/scorecard/newPlayer.js
@@ -1,10 +1,13 @@
-import { useState, useRef } from "preact/hooks";
+import { useState, useRef, useEffect } from "preact/hooks";
 import UniqueGenerator from "../../lib/board";
 import { generate } from 'project-name-generator';
 
 const NewPlayer = ({addPlayer}) => {
 
     const [name, setName] = useState(generate().spaced);
+    const [randomCard, setRandomCard] = useState(UniqueGenerator.randomize());
+    const [focussed, setFocussed] = useState(false);
+
     const inputRef = useRef();
 
     const submit = (e) => {
@@ -14,17 +17,40 @@ const NewPlayer = ({addPlayer}) => {
             return;
         }
 
-        addPlayer({name, card: UniqueGenerator.randomize()})
-        setName(generate().spaced);
+        addPlayer({name, card: randomCard});
+
+        setFocussed(() => true);
+        setName(() => generate().spaced);
+        setRandomCard(() => UniqueGenerator.randomize());
+    }
+
+    const hasFocus = (focussed) => {
+        setFocussed(() => focussed);
+        focussed && inputRef.current.select();
+    };
+
+    useEffect(() => {
+        if (! focussed) {
+            return;
+        }
 
         inputRef.current.focus();
-    }
+        inputRef.current.select();
+    }, [name]);
 
     return (
         <div>
             <form onSubmit={submit}>
-                <input ref={inputRef} value={name} onFocus={(e) => e.target.select()} onInput={(f) => setName(f.target.value)} placeholder="Name" />
+                <label>
+                    Name
+                    <input ref={inputRef} value={name} onFocus={() => hasFocus(true)} onInput={(f) => setName(f.target.value)} placeholder="Name" />
+                </label>
                 <button type="submit">Add</button>
+                <br />
+                <label>
+                    Card ID *
+                    <input value={randomCard} onFocus={(e) => e.target.select()} onInput={(f) => setRandomCard(f.target.value)} placeholder="Card ID" />
+                </label>
             </form>
         </div>
     );
