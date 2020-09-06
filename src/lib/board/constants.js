d9c8e662fe2882c2d9d4b8873a9ad47819ebd9c0
Jeroen Boersma <jeroen@elgentos.nl>
2020-09-06 11:05:29 +0200

    Formating in constants file

--- src/lib/board/constants.js
+++ src/lib/board/constants.js
@@ -1,8 +1,8 @@
 
-export const MAXFIELDS = 75;
-export const NUMBERS = new Array(MAXFIELDS).fill(0).map((v, i) => i+1);
+export const MAXFIELDS  = 75;
+export const NUMBERS    = new Array(MAXFIELDS).fill(0).map((v, i) => i+1);
 export const EMTPYFIELD = '';
 
-export const COLUMNS = 5;
-export const ROWS = 5;
-export const EXCLUDE = {2: {2: true}};
+export const COLUMNS    = 5;
+export const ROWS       = 5;
+export const EXCLUDE    = {2: {2: true}};
