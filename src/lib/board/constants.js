1bdf4c620d56e86662070c747fc5cede1d3074b5
Jeroen Boersma <jeroen@elgentos.nl>
2020-09-06 11:00:53 +0200

    dded max fields as a constant

--- src/lib/board/constants.js
+++ src/lib/board/constants.js
@@ -1,5 +1,6 @@
 
-export const NUMBERS = new Array(75).fill(0).map((v, i) => i+1);
+export const MAXFIELDS = 75;
+export const NUMBERS = new Array(MAXFIELDS).fill(0).map((v, i) => i+1);
 export const EMTPYFIELD = '';
 
 export const COLUMNS = 5;
