9f6c87d804b3e8b3f0c762e30f584f12e522e9de
Jesse de Boer <jesse@elgentos.nl>
2021-07-09 16:57:49 +0200

    Package update including new cli, postcss and tailwind

--- preact.config.js
+++ preact.config.js
@@ -1,13 +1,14 @@
 module.exports = (config, env, helpers) => {
+  if (!config) throw new Error(notFoundError("config"));
+  if (!env) throw new Error(notFoundError("env"));
+  if (!helpers) throw new Error(notFoundError("helpers"));
 
-  const postCssLoaders = helpers.getLoadersByName(config, 'postcss-loader');
-
+  const postCssLoaders = helpers.getLoadersByName(config, "postcss-loader");
   postCssLoaders.forEach(({ loader }) => {
-    const plugins = loader.options.plugins;
+    const plugins = loader.options.postcssOptions.plugins;
 
     // Add tailwind css at the top.
-    plugins.unshift(require('tailwindcss'));
+    plugins.unshift(require("tailwindcss"));
   });
-
   return config;
 };
